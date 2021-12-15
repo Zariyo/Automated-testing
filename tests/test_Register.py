3796e9cb5f4d81ce5dc7f66ac4a5891b45691feb
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2021-12-15 18:56:02 +0100

    test id as int

--- tests/test_Register.py
+++ tests/test_Register.py
@@ -14,3 +14,6 @@ class test_Register(unittest.TestCase):
 
     def test_addStudent_exception_studentExists(self):
         self.assertRaises(Exception, self.tmp.add_Student, 2, 'Krzysztof', 'Kowal')
+
+    def test_addStudent_exception_id_not_int(self):
+        self.assertRaises(ValueError, self.tmp.add_Student, "abc", 'Adam', 'Nowak')
