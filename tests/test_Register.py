0300fc3959f203e0f41b222bf2e8982c1581e8c6
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2021-12-15 19:47:58 +0100

    test removeStudent

--- tests/test_Register.py
+++ tests/test_Register.py
@@ -8,6 +8,7 @@ class test_Register(unittest.TestCase):
         register = Register([])
         self.tmp = register
         self.tmp.add_Student(2, 'Krzysztof', 'Kowal')
+        self.tmp.add_Student(28, 'Adam', 'Nowak')
 
     def test_addStudent(self):
         self.assertEqual({"id": 1, "name": "Adam", "surname": "Kowal"},
@@ -64,3 +65,6 @@ class test_Register(unittest.TestCase):
 
     def test_editStudent_exception_surname_not_string(self):
         self.assertRaises(TypeError, self.tmp.edit_Student, 15, None, "Adam", 123)
+
+    def test_removeStudent(self):
+        self.assertEqual({"id": 28, "name": "Adam", "surname": "Nowak"}, self.tmp.removeStudent(28))
