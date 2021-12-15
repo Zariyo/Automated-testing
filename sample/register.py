cd7a0fd444b82be4b50d1176c684ecccffd0ca8d
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2021-12-15 19:37:35 +0100

    Update to pass tests

--- sample/register.py
+++ sample/register.py
@@ -31,6 +31,20 @@ class Register:
             raise Exception("Student ID already taken. Perhaps the student is already in the register?")
 
     def edit_Student(self, studId, updateId=None, updateName=None, updateSurname=None):
+        if type(studId) is not int:
+            if type(studId) in [float, str]:
+                studId = int(float(str(studId)))
+            else:
+                raise ValueError("Id must be an integer")
+        if type(updateName) is not str:
+            raise TypeError("Student name must be a string")
+        if type(updateSurname) is not str:
+            raise TypeError("Student surname must be a string")
+        if type(updateId) is not int and updateId is not None:
+            if type(updateId) in [float, str]:
+                updateId = int(float(str(updateId)))
+            else:
+                raise ValueError("Updated Id must be an integer")
         for stud in self.students:
             if stud['id'] is studId:
                 if updateId is not None:
